0657c510871d84d946b7b10bd9cef7ccd2b19cbb
RexYu <yu.chenhang@gmail.com>
2022-02-07 14:57:01 +0800

    修正 CustomAuthenticationProvider.authenticate() 回傳值

--- custom-authorization/src/main/java/tw/com/rex/oauth2/config/CustomAuthenticationProvider.java
+++ custom-authorization/src/main/java/tw/com/rex/oauth2/config/CustomAuthenticationProvider.java
@@ -2,7 +2,6 @@ package tw.com.rex.oauth2.config;
 
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,7 +17,7 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String casToken = (String) authentication.getCredentials();
         UserDetails userDetails = userDetailsService.loadUserBySsoToken(casToken);
-        return new UsernamePasswordAuthenticationToken(userDetails, casToken, userDetails.getAuthorities());
+        return new CustomAuthenticationToken(userDetails, casToken, userDetails.getAuthorities());
     }
 
     @Override
